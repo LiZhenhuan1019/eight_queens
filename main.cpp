3e45fb7fe9366d5e4cb15d4ac03a579704cb8eb8
LiZhenhuan1019 <lizhenhuan1019@qq.com>
2016-10-17 21:56:38 +0800

    refactor a little

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+
 class eight_queens
 {
 public:
-    eight_queens(int chess_board_size,std::ostream& ostream = std::cout)
-        :size(chess_board_size),ostream(ostream)
+    eight_queens(int chess_board_size, std::ostream& ostream = std::cout)
+        : size(chess_board_size), ostream(ostream)
     {
         queen_positions.reserve(size);
     }
@@ -17,13 +18,13 @@ public:
 private:
     void fill_and_print_queen_poisitions_impl()
     {
-        if(queen_positions.size() == size)
+        if (queen_positions.size() == size)
         {
             print_all_queen_poisitions();
-            return ;
+            return;
         }
-        for(int pos = 0;pos<size;++pos)
-            if(queen_position_is_legal(pos))
+        for (int pos = 0; pos < size; ++pos)
+            if (queen_position_is_legal(pos))
             {
                 queen_positions.push_back(pos);
                 fill_and_print_queen_poisitions_impl();
@@ -33,37 +34,41 @@ private:
     bool queen_position_is_legal(int new_x)
     {
         int new_y = queen_positions.size();
-        for(int old_y = 0; old_y < queen_positions.size();++old_y)
+        for (int old_y = 0; old_y < queen_positions.size(); ++old_y)
         {
             int old_x = queen_positions[old_y];
-            if(old_x== new_x || std::abs(new_x - old_x) == new_y - old_y)
+            if (can_touch(new_x, new_y, old_x, old_y))
                 return false;
         }
         return true;
     }
-
+    bool can_touch(int new_x, int new_y, int old_x, int old_y)
+    {
+        return old_x == new_x || std::abs(new_x - old_x) == new_y - old_y;
+    }
     void print_all_queen_poisitions()
     {
-        ostream<<"chess "<<++counts<<'\n';
-        for(int line = 0;line < queen_positions.size();++line)
+        ostream << "solution " << ++counts << '\n';
+        for (int line = 0; line < queen_positions.size(); ++line)
             print_line(line);
-        ostream<<'\n';
+        ostream << '\n';
     }
     void print_line(int line)
     {
         int pos_x = queen_positions[line];
-        for(int x = 0;x<pos_x;++x)
-            ostream<<'0';
-        ostream<<'1';
-        for(int x = pos_x+1;x<size;++x)
-            ostream<<'0';
-        ostream<<'\n';
+        for (int x = 0; x < pos_x; ++x)
+            ostream << '0';
+        ostream << '1';
+        for (int x = pos_x + 1; x < size; ++x)
+            ostream << '0';
+        ostream << '\n';
     }
     const int size;
     std::vector<int> queen_positions;
     std::ostream& ostream;
     std::size_t counts = 0;
 };
+
 int main()
 {
     eight_queens queens(8);
